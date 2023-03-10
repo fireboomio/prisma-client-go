706b850559a27593892602a95eb790710f374314
xufeixiang <xufeixiang@ainanjing.org.cn>
2023-03-10 11:17:59 +0800

    fix: remove replace bytes with string for 'Blob'.

--- engine/introspection/me.go
+++ engine/introspection/me.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/prisma/prisma-client-go/binaries"
@@ -87,98 +86,6 @@ func (e *IntrospectEngine) ensure() (string, error) {
 
 	return file, nil
 }
-
-//func (e *IntrospectEngine) Pull(schema string) (string, error) {
-//	startParse := time.Now()
-//
-//	ctx, cancel := context.WithTimeout(context.Background(), time.Second*600)
-//	defer cancel()
-//
-//	cmd := exec.CommandContext(ctx, e.path)
-//
-//	pipe, err := cmd.StdinPipe() // 标准输入流
-//	if err != nil {
-//		return "", fmt.Errorf("introspect engine std in pipe %v", err.Error())
-//	}
-//	defer pipe.Close()
-//	// 构建一个json-rpc 请求参数
-//	req := IntrospectRequest{
-//		Id:      1,
-//		Jsonrpc: "2.0",
-//		Method:  "introspect",
-//		Params: []map[string]interface{}{
-//			{
-//				"schema":             string(schema),
-//				"compositeTypeDepth": -1,
-//			},
-//		},
-//	}
-//
-//	data, err := json.Marshal(req)
-//	if err != nil {
-//		return "", err
-//	}
-//	// 入参追加到管道中
-//	_, err = pipe.Write(append(data, []byte("\n")...))
-//	if err != nil {
-//		// return "", err
-//		return "", err
-//	}
-//
-//	out, err := cmd.StdoutPipe()
-//	if err != nil {
-//		err = fmt.Errorf("Introspect std out pipe %s ", err.Error())
-//	}
-//	r := bufio.NewReader(out)
-//
-//	// 开始执行
-//	err = cmd.Start()
-//	if err != nil {
-//		return "", err
-//	}
-//
-//	//var response IntrospectResponse
-//	var response IntrospectResponse
-//
-//	outBuf := &bytes.Buffer{}
-//	// 这一段的意思是，每100ms读取一次结果，直到超时或有结果
-//	for {
-//		// 等待100 ms
-//		//time.Sleep(time.Millisecond * 100)
-//		b, err := r.ReadByte()
-//		if err != nil {
-//			err = fmt.Errorf("Introspect ReadByte %s ", err.Error())
-//		}
-//		err = outBuf.WriteByte(b)
-//		if err != nil {
-//			err = fmt.Errorf("IntrospectwriteByte %s ", err.Error())
-//		}
-//
-//		if b == '\n' {
-//			// 解析响应结果
-//			err = json.Unmarshal(outBuf.Bytes(), &response)
-//			if err != nil {
-//				return "", err
-//			}
-//			if response.Error == nil {
-//				log.Println("introspect successful")
-//			}
-//			fmt.Print("ende ")
-//			break
-//		}
-//		// 如果超时了？跳出读取？
-//		if err := ctx.Err(); err != nil {
-//			return "", err
-//		}
-//	}
-//	log.Printf("[timing] introspect took %s", time.Since(startParse))
-//	if response.Error != nil {
-//		return "", fmt.Errorf("introspect error: %s", response.Error.Data.Message)
-//	}
-//	dataModel := strings.Replace(response.Result.DataModel, " Bytes", " String", -1)
-//	//dataModel := strings.Replace(response.Result.DataModel, " Bytes", " String", -1)
-//	return dataModel, nil
-//}
 
 func (e *IntrospectEngine) Pull(schema string) (string, error) {
 	startParse := time.Now()
@@ -258,7 +165,5 @@ func (e *IntrospectEngine) Pull(schema string) (string, error) {
 	}
 	log.Println("introspect successful")
 
-	dataModel := strings.Replace(response.Result.DataModel, " Bytes", " String", -1)
-	//dataModel := strings.Replace(response.Result.DataModel, " Bytes", " String", -1)
-	return dataModel, nil
+	return response.Result.DataModel, nil
 }
